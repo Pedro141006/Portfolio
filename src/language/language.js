3cb876faabe6ca1f40e0b91562a366106c0b594c
Pedro Henrique <phpdef@gmail.com>
2024-10-24 16:06:51 -0400

    add more containers sections, translations

--- src/language/language.js
+++ src/language/language.js
@@ -5,7 +5,8 @@ const language = {
       introd: "I'm a beginner developer looking for my first opportunity.",
       workTo: 'Let’s work together',
       goal: "As an aspiring developer, my goal is to gain practical knowledge, software development experience and best practices. I'm committed looking for my first job opportunity to apply my skills skills and contribute to the development of innovative solutions, while time that I continue to improve my technical and collaborative skills.",
-      title_goal: 'My goal:'
+      title_goal: 'My goal:',
+      title_tech: 'My technologies:'
     },
     pt_br: {
       hello: 'Olá! <br /> Eu sou Pedro',
@@ -13,7 +14,8 @@ const language = {
         'Sou um desenvolvedor iniciante em busca da minha primeira oportunidade.',
       workTo: 'Vamos trabalhar juntos',
       goal: 'Como aspirante a desenvolvedor, meu objetivo é adquirir conhecimento prático experiência e práticas recomendadas de desenvolvimento de software. Estou empenhado em procurar minha primeira oportunidade de emprego para aplicar as minhas competências e contribuir para o desenvolvimento de soluções inovadoras, ao mesmo tempo que continuo a melhorar as minhas competências técnicas e colaborativas.',
-      title_goal: 'Meu objetivo:'
+      title_goal: 'Meu objetivo:',
+      title_tech: 'Minhas tecnologias:'
     }
   }
 }
@@ -23,6 +25,7 @@ let $workTo = document.querySelector('.text')
 let $goal = document.querySelector('.goal')
 let $hello = document.querySelector('.hello')
 let $title_goal = document.querySelector('.title-goal')
+let $title_tech = document.querySelector('.techTitle')
 
 let $selectLang = document.getElementById('languages')
 
@@ -37,11 +40,13 @@ const setLanguage = text => {
     $workTo.innerHTML = language.lang.en.workTo
     $goal.innerHTML = language.lang.en.goal
     $title_goal.innerHTML = language.lang.en.title_goal
+    $title_tech.innerHTML = language.lang.en.title_tech
   } else if (text == 'pt-br') {
     $intro.innerHTML = language.lang.pt_br.introd
     $hello.innerHTML = language.lang.pt_br.hello
     $workTo.innerHTML = language.lang.pt_br.workTo
     $goal.innerHTML = language.lang.pt_br.goal
     $title_goal.innerHTML = language.lang.pt_br.title_goal
+    $title_tech.innerHTML = language.lang.pt_br.title_tech
   }
 }
